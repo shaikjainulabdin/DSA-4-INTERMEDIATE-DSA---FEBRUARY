8ebf8d8198937e3b59e29f426778570d37780a3e
shaikjainulabdin <54411936+shaikjainulabdin@users.noreply.github.com>
2023-02-26 17:03:42 +0530

    Create Check whether K-th bit is set or not.java

---
+++ Bit manupilation/Check whether K-th bit is set or not.java
@@ -0,0 +1,42 @@
+//    https://course.acciojob.com/idle?question=90266d47-b043-4a66-b6c8-57d12147a33d
+
+
+import java.io.*;
+import java.util.*;
+
+class Main
+{
+    public static void main(String args[])throws IOException
+    {
+        Scanner sc = new Scanner(System.in);
+        
+        int N = sc.nextInt();
+        int K=sc.nextInt();
+        
+
+        Solution obj=new Solution();
+        Boolean ans=obj.solve(N,K);
+
+        if(ans==true)
+            System.out.println("YES");
+        else
+            System.out.println("NO");
+        
+    }
+}
+
+class Solution{
+    
+    public static Boolean solve(int n,int k)
+    {
+        // Your code here
+		int mask = 1<< k;
+		if((n & mask) > 0)
+		{
+			return true;
+		}
+		else {
+			return false;
+		}
+    }
+}
